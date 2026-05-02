e7330568f0c1201c3e092a13bad987cb23320ea4
YourNameShadow <test@gmail.com>
2026-05-02 17:38:05 +0600

    add farcaster manifest

---
+++ minikit.config.ts
@@ -0,0 +1,8 @@
+export default {
+  name: "StreakBase",
+  description: "Daily streak tracker on Base",
+  url: "https://streak-miniapp-synthesiss-5982s-projects.vercel.app",
+  iconUrl: "https://streak-miniapp-synthesiss-5982s-projects.vercel.app/icon.png",
+  splashImageUrl: "https://streak-miniapp-synthesiss-5982s-projects.vercel.app/splash.png",
+  splashBackgroundColor: "#0a0a0f",
+}
